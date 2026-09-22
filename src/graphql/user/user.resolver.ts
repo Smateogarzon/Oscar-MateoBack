1deb39e4607e9aa460b43313eda41e1297cf128d
Smateogarzon <mateogarzon1002@gmail.com>
2026-09-22 17:27:23 -0500

    Refactor code structure for improved readability and maintainability

--- src/graphql/user/user.resolver.ts
+++ src/graphql/user/user.resolver.ts
@@ -81,6 +81,15 @@ export class UserResolver {
 
   @Mutation(() => UserObjectType)
   @RequirePermissions(PermissionCode.USERS_MANAGE)
+  activateUser(
+    @CurrentCompanyId() companyId: string,
+    @Args('id', { type: () => ID }) id: string,
+  ) {
+    return this.userService.activate(companyId, id);
+  }
+
+  @Mutation(() => UserObjectType)
+  @RequirePermissions(PermissionCode.USERS_MANAGE)
   resetUserPassword(
     @CurrentCompanyId() companyId: string,
     @Args('id', { type: () => ID }) id: string,
